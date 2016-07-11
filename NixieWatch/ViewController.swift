dd67dc154a1b033005bfd0b963510e9f79bb74e4
Bruno Philipe <brunophilipe@me.com>
2016-07-10 23:31:07 -0300

    Implements basic companion app

--- NixieWatch/ViewController.swift
+++ NixieWatch/ViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-	override func viewDidLoad() {
+	override func viewDidLoad()
+	{
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
-	override func didReceiveMemoryWarning() {
+	override func didReceiveMemoryWarning()
+	{
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	override func preferredStatusBarStyle() -> UIStatusBarStyle
+	{
+		return .LightContent
+	}
 
-
+	@IBAction func didTapVisitRepoButton(sender: AnyObject)
+	{
+		if let url = NSURL(string: "https://github.com/brunophilipe/NixieWatch")
+		{
+			UIApplication.sharedApplication().openURL(url)
+		}
+	}
 }
 
